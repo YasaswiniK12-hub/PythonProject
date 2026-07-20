0897c09f50b7d7d96acf7b032a70f0e3c8746e3e
YasaswiniK12-hub <yasaswinikosuru12@gmail.com>
2026-07-20 17:18:22 +0530

    I built a basic calculator.

---
+++ Basic_Calculator.py
@@ -0,0 +1,11 @@
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result= num1+num2
+print(result)
+# remember your'e lesson input() string . Therefore, Concatenation happened not addition
+# result = int(num1)+ int(num2)
+print(result)
+# what if they are float
+result= float(num1)+float(num2)
+print(result)
