3f0b1736cf48bad3d22ee33e2aff2150f36c818d
YasaswiniK12-hub <yasaswinikosuru12@gmail.com>
2026-07-20 13:15:01 +0530

    Getting Input From Users

---
+++ Getting_Input_From_Users.py
@@ -0,0 +1,7 @@
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello "+ name+"! You are "+ age)
+# ok so youv'e learnt that string-string concatenation is valid not string to int
+# why did the code work age is int right
+# keep in mind input() function returns string it's not 25 it's "25"
