f92b3b36aa88d176582add5bbd2ef47d73740677
YasaswiniK12-hub <yasaswinikosuru12@gmail.com>
2026-07-20 12:58:17 +0530

    Working with Numbers

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from math import *
 
 name="Hazel"
 age=15
@@ -29,3 +30,37 @@ print(phrase[3])
 # access index with the help of letter
 print(phrase.index("Dig"))
 print(phrase.replace("Digital","Lovely"))
+
+# Working with Numbers
+print(2)
+print(2.9087)
+print(-23)
+print(3+4.5)
+print(3*4+5)
+print(3*(4+5)) # Handles math operations
+#mod operation
+print(10%3) # spits out the reminder
+num=-5
+print(num)
+#convert a number into a string
+print(str(num)+" my favourite number")
+# you wanna  place a number along with a string then this function comes handy : str()
+# abs()- absolute value
+print(abs(num))
+# pow()- power base, exponent
+print(pow(4,2))
+# max()- maximum number given
+print(max(4,6))
+# min() -minimum number given
+print(min(4,6))
+# round()- round a number
+print(round(3.14))
+# so yeah you have a bunch of math functions . Inorder to use that you have to access it from math library
+# at the top you have it go check it
+print(floor(3.7))
+# floor()- chops of the decimal. Pick the lowest value
+print(ceil(3.7))
+# ceil()- picks the greatest value
+print(sqrt(36))
+# sqrt()- square root
+
