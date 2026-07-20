562c9109419cfb8a2dc7530805a7e91d91b34f36
YasaswiniK12-hub <yasaswinikosuru12@gmail.com>
2026-07-20 23:36:33 +0530

    Tuples - The Immutable

---
+++ Tuples.py
@@ -0,0 +1,8 @@
+# Tuple is a type of data structure. Has few key differences from list
+coordinates=[(4,5),(6,7),(80,34)]
+# Tupple is Immutable . round brackets . List square brackets
+#coordinates[1]=10
+# TypeError: 'tuple' object does not support item assignment
+print(coordinates[1])
+#List Mutable
+# In real life scenarios they use tuples when they are sure that this doesn't need modification at any point of time
