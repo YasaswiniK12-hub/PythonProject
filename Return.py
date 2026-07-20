926916730ba03a8ceea915bc8cd5b90a3066885a
YasaswiniK12-hub <yasaswinikosuru12@gmail.com>
2026-07-20 23:36:15 +0530

    Return - we get information back from the function

---
+++ Return.py
@@ -0,0 +1,9 @@
+# Inorder to get the information back from function return keyword comes into the picture. Allows python to return info from a function
+# cube a number
+def cube(num):
+    return num*num*num
+
+print(cube(3)) # This gives None when you don't use return keyword
+# lines after return statement such a print line nah you're not getting it. RETURN ---> OUT FROM THE FUNCTION
+
+
