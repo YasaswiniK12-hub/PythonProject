593b8e174e043f0eda1dc09b91e6d24b13878b23
YasaswiniK12-hub <yasaswinikosuru12@gmail.com>
2026-07-22 12:28:25 +0530

    Fundamentals of if, else elif.

--- If_Statements.py
+++ If_Statements.py
@@ -1,4 +1,12 @@
-is_female = True
+is_female = False
+is_tall = False
+if is_female and is_tall:
+    print("Female or tall or both")
+elif is_female and not(is_tall):
+    print("Short Female");
+elif not(is_female) and is_tall:
+    print("Not female but tall")
+else:
+    print("Neither a female nor tall")
 
-if(is_female):
-    print("Female")
+# try replacing or with and, and observe the output
