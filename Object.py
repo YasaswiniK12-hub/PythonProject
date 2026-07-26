b4f304d1765a89039f0f6659ccf706fba8d2b411
YasaswiniK12-hub <yasaswinikosuru12@gmail.com>
2026-07-26 18:45:07 +0530

    Object executing all methods

---
+++ Object.py
@@ -0,0 +1,14 @@
+from Student import Student
+# see what I have learnt is from the Student file take Student class
+student1=Student("Ruskin","Math",4.2,False)
+student2=Student("James","Physics",3.9,True)
+print(student1.name)
+print(student1.major)
+print(student1.gpa)
+print(student1.is_on_probation)
+print(student2.name)
+print(student2.major)
+print(student2.gpa)
+print(student2.is_on_probation)
+
+# from file_name import class_name
