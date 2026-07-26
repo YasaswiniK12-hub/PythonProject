4e1da83cb7c11f11555d49a0d13d419d48ba331d
YasaswiniK12-hub <yasaswinikosuru12@gmail.com>
2026-07-26 18:47:54 +0530

    Inheritance object

---
+++ ChefObject.py
@@ -0,0 +1,14 @@
+from Chinese_Chef import Chinese_Chef
+
+myChineseChef = Chinese_Chef()
+
+myChineseChef.make_fried_rice()
+myChineseChef.make_salad()
+myChineseChef.make_chicken()
+
+# This is the proper implementation of the concept INHERITANCE.
+# so when you want to inherit this is what you do .
+'''
+1) from file_name import class_name
+2) class class_name(class you want to import):
+'''
