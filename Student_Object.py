2d67936ca7ff762e3c648d29ab91070965805f5b
YasaswiniK12-hub <yasaswinikosuru12@gmail.com>
2026-07-26 18:43:06 +0530

    Object

---
+++ Student_Object.py
@@ -0,0 +1,7 @@
+from Object_Functions import Student
+
+student1=Student("Yasaswini","Physics",4.9)
+student2=Student("Lauv","Maths",3.1)
+
+print(student1.is_honor_roll())
+print(student2.is_honor_roll())
