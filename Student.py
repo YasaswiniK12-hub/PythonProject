1370a1b96faf7b0a4b56b5390a70f61e06310481
YasaswiniK12-hub <yasaswinikosuru12@gmail.com>
2026-07-26 18:42:54 +0530

    Class

---
+++ Student.py
@@ -0,0 +1,9 @@
+class Student:
+
+     def __init__(self,name,major,gpa,is_on_probation):
+         self.name=name
+         self.major=major
+         self.gpa=gpa
+         self.is_on_probation=is_on_probation
+# so yeah basically  the class name is Student . Attributes are self.variable_name . name,major,gpa,is_on_probation are the values we passed from the object.
+#__init__ -> Initialize
