5d0361aa78a6bb1afe35abe5e99864938839744e
YasaswiniK12-hub <yasaswinikosuru12@gmail.com>
2026-07-26 18:48:02 +0530

    Inheritance parent class

---
+++ Chef.py
@@ -0,0 +1,18 @@
+class Chef:
+    def make_chicken(self):
+        print("Chef makes chicken.")
+    def make_salad(self):
+        print("Chef makes salad.")
+    def make_special_dish(self):
+        print("Chef makes special dish.")
+
+
+
+
+
+
+
+
+
+
+# REMEMBER SELF MEANS THIS EXACT OBJECT WHAT YOU'RE CURRENTLY WORKING WITH . ALLOWS THE OBJECT TO ACCESS IT'S OWN ATTRIBUTES AND METHODS. SERVES THE SAME PURPOSE AS THE .this IN JAVA
