5cb1ba4a1e7e7935f580e976aa6a4393d2c5c17f
YasaswiniK12-hub <yasaswinikosuru12@gmail.com>
2026-07-26 09:49:43 +0530

    Imported useful_tools.

---
+++ Modules_and_PIP.py
@@ -0,0 +1,11 @@
+# module is a python file which I can import into current file.
+# I can access everything in module
+import useful_tools
+
+print(useful_tools.roll_dice(10))
+# Go  check out list of python modules
+# proper documentation for each module . python.org
+
+# pip is a standard package manager for the python that allows install, update and manage additional libraries and dependencies that are not included in the standard python library
+
+
